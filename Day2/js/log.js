29357d3d4bc64bbe3da84e3513ac9b831ddcaedb
stepanlyalkovski <stepanlyalkovski@gmail.com>
2016-08-11 17:12:57 +0300

    add semicolon in func Day 2

--- Day2/js/log.js
+++ Day2/js/log.js
@@ -4,9 +4,9 @@ for (var i = 1; i <= typeCount; i++) {
 	var method = "getCount" + i;
 
 	for (var j = 0; j < objArray.length; j++) {	
-		var test =  objArray[j][method] || function()  { return 0 };
+		var test =  objArray[j][method] || function()  { return 0; };
 		counter += test.call(objArray[j]);
 	}
-	
+
 	console.log("count{%d}={%d}", i, counter);
 }
