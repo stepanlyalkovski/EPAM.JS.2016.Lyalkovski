c332c64c9e05721e6c8cdfd585d4d8d48e279db0
stepanlyalkovski <stepanlyalkovski@gmail.com>
2016-08-11 17:03:04 +0300

    complete Day 2 without if statements

--- Day2/js/log.js
+++ Day2/js/log.js
@@ -2,12 +2,11 @@ var typeCount = 3;
 for (var i = 1; i <= typeCount; i++) {
 	var counter = 0;
 	var method = "getCount" + i;
+
 	for (var j = 0; j < objArray.length; j++) {	
-		if (objArray[j][method] === undefined) {
-			continue;
-		} else {
-			counter += objArray[j][method]();
-		}
+		var test =  objArray[j][method] || function()  { return 0 };
+		counter += test.call(objArray[j]);
 	}
+	
 	console.log("count{%d}={%d}", i, counter);
 }
