d90a744aecd4ba5ee705f029fb498855537c3a6e
stepanlyalkovski <stepanlyalkovski@gmail.com>
2016-08-11 17:07:26 +0300

    remove optional Day1 function

--- Day1/js/calc.js
+++ Day1/js/calc.js
@@ -12,12 +12,3 @@ for(var i = 0; i < data.length; i++){
 
 printArray();
 
-/*test function */
-function check(arg){
-    if(arg){
-        return "if - true";
-    } else {
-        return "if - false";
-    }
-}
-
