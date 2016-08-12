00225e73b60f3b8019c414a616ec3e7411524ce2
stepanlyalkovski <stepanlyalkovski@gmail.com>
2016-08-12 18:10:54 +0300

    add demo events

--- BigTask/js/test.js
+++ BigTask/js/test.js
@@ -1,21 +1,49 @@
 var game = {};
 game.isStopped = true;
-game.btnStart = document.getElementById("btn-play");
-/* Playground size - image size */
-game.position = {Xmax: 700 - 64, Ymax: 400 - 64};
 
-function getRandomArbitrary(max, min){
+function ChangeState() {
+    var color;
+    var text;
+    if(game.isStopped){
+        game.timeId = setInterval(ChangePosition, 2000);
+        color = "red";
+        text = "Stop";
+    } else {
+        clearTimeout(game.timeId);
+        color = "green";
+        text = "Start";
+    }
+    game.isStopped = !game.isStopped; // change state
+    game.controlBtn.style.background = color;
+    game.controlBtn.innerHTML = text;
+}
+
+function getRandomArbitrary(max, min) {
     return Math.random() * (max - min) + min;
 }
 
-function getRandPosition(){
-    return getRandomArbitrary(0, 80);
-}
-
-function ChangePosition(){
-    var items = document.getElementsByClassName("resource-item");
+function ChangePosition() {
+    var items = $(".resource-item");
     for(var i = 0; i < items.length; i++){
         items[i].style.top = getRandomArbitrary(0, game.position.Ymax) + "px";
         items[i].style.left = getRandomArbitrary(0, game.position.Xmax) + "px";
     }
 }
+
+$(document).ready(function() {
+
+    game.btnStart = document.getElementById("btn-play");
+    /* Playground size - image size */
+    game.position = {Xmax: 700 - 64, Ymax: 400 - 64};
+    game.controlBtn = $("#btn-control").get(0);
+    game.controlBtn.style.backgroundcolor = "green";
+    game.controlBtn.text = "Start";
+    $("#btn-control").click(function () {
+        ChangeState();
+    });
+
+});
+
+
+
+
